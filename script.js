61ec3ebdbb7587f10088016a4dddc0e2ace76801
Iker Vélez <168359086+Iker1211@users.noreply.github.com>
2025-02-24 17:19:23 +0000

    feat: Add logic for display books in each row

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-const form_btn = document.getElementById("add-book");
+/* Logic for displaying the books */
 
-form_btn.onclick = () => {
-    console.log('y ahora');
-};
+const form = document.getElementById("form");
 
+let main = document.querySelector("main");
+let table = document.createElement("table");
+main.appendChild(table);
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+    let tr_elem = document.createElement("tr");
+
+    for (var i = 0; i < 5; i++) {
+        trFunction(i);
+    }
+
+    function trFunction(number) {
+        var td_elem = document.createElement("td");
+        td_elem.textContent = 'Hola';
+    
+        tr_elem.appendChild(td_elem);
+        table.appendChild(tr_elem);
+        // table.appendChild(td_elem);
+    }
+});
+
+
+/* Book structures that hold all information */
