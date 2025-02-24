2556afeb3f45cf1a1a2e3fd83873238ca4033055
Iker Vélez <168359086+Iker1211@users.noreply.github.com>
2025-02-24 22:32:45 +0000

    fix: Add ghost table to add theader with same bootstrap classes

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 /* Logic for displaying the books */
-
 const form = document.getElementById("form");
 
 let main = document.querySelector("main");
 let table = document.createElement("table");
+let tbody = document.createElement("tbody");
+let thead = document.createElement("thead");
+
 main.appendChild(table);
+table.appendChild(thead)
+table.appendChild(tbody);
+
+function setAttributes(el, attrs) {
+    for (var key in attrs) {
+        el.setAttribute(key, attrs[key]);
+    }
+}
+
+setAttributes(table, {"class": "table table-light"}); // Adding Bootstrap classes //
 
 form.addEventListener("submit", function(event) {
 
     event.preventDefault();
-    let tr_elem = document.createElement("tr");
 
-    for (var i = 0; i < 5; i++) {
-        trFunction(i);
-    }
+    let tr_elem = document.createElement("tr");
 
     function trFunction(number) {
         var td_elem = document.createElement("td");
-        td_elem.textContent = 'Hola';
-    
+        td_elem.textContent = 'Hola' + number;
+
         tr_elem.appendChild(td_elem);
-        table.appendChild(tr_elem);
-        // table.appendChild(td_elem);
+        tbody.appendChild(tr_elem);
+
+        console.dir(td_elem);
+    }
+
+    for (var i = 0; i < 5; i++) {
+        trFunction(i);
     }
 });
 
