2fcd17b9d994fcdcab8d9576848f7a2cf1e62a29
Iker Vélez <168359086+Iker1211@users.noreply.github.com>
2025-02-27 02:47:04 +0000

    feat: Add updateLocalStorage and checkLocalSotrage functions to toggle read status

--- script.js
+++ script.js
@@ -19,38 +19,51 @@ setAttributes(table, {"class": "table table-light"}); // Adding Bootstrap classe
 const listener = form.addEventListener("submit", function(event) {
 
     event.preventDefault();
-
     statement.style.display = 'none';
 
     addBookToLibrary();
     empty_form();
     render();
-    console.log(my_library);
-    // let tr_elem = document.createElement("tr");
-    // console.log(tr_elem);
-
-    /* ForEach */
-    // function trFunction(number) {
-
-    //     var td_elem = document.createElement("td");
-    //     td_elem.textContent = 'Hola' + i; // //
-
-    //     tr_elem.appendChild(td_elem);
-    //     tbody.appendChild(tr_elem);
-    // }
-
-    // for (var i = 0; i < 5; i++) { /* Loops through the array */
-    //     trFunction(i);
-    // }
-
-    // let last_td = tr_elem.lastChild;
-
-    // addBookToLibrary();
-    // console.table(myLibrary);
 });
 
-/* Book structures that hold all information */
+table.addEventListener("click", (e) => {
 
+    const currentTarget = e.target.parentNode.parentNode.childNodes[1];
+    if (e.target.innerHTML == "delete") {
+      if (confirm(`are you sure you want to delete ${currentTarget.innerText}`))
+        deleteBook(findBook(my_library, currentTarget.innerText));
+    }
+    if (e.target.classList.contains("status-button")) {
+        changeStatus(findBook(my_library, currentTarget.innerText));
+    }
+    updateLocalStorage();
+    render();
+});
+
+function empty_form() {
+    $name.value = '';
+    $author.value = '';
+    $nr_pages.value = '';
+    $status.value = 'read';
+   } 
+   
+function render() {
+    tbody.innerHTML = "";
+    my_library.forEach((book) => {
+        const htmlBook = `
+        <tr>
+            <td>${book.name}</td>
+            <td>${book.author}</td>
+            <td>${book.nr_pages}</td>
+            <td><button class="btn btn-success status-button">${book.status}</button></td>
+            <td><button class="btn btn-danger">delete</button></td>
+        </tr>
+        `;
+        tbody.insertAdjacentHTML("afterbegin", htmlBook);
+    });
+   }
+
+/* Book structures that hold all information */
 const my_library = [];
 
 const $name = document.getElementById("title");
@@ -70,39 +83,43 @@ class Book {
 function addBookToLibrary() {
     const new_book = new Book($name.value, $author.value, $nr_pages.value, $status.value);
     my_library.push(new_book);
+    updateLocalStorage();
 }
 
-function chageStatus(book) {
-    if (library[book].status === "read") {
-        library[book].status = "not read";
-      } else library[book].status = "read";
+function changeStatus(book) {
+    if (my_library[book].status === "read") {
+        my_library[book].status = "not read";
+      } else my_library[book].status = "read";
 }
 
-function empty_form() {
- $name.value = '';
- $author.value = '';
- $nr_pages.value = '';
- $status.value = 'read';
-} 
-
-function render() {
-    // checkLocalStorage();
-    tbody.innerHTML = "";
-    my_library.forEach((book) => {
-      const htmlBook = `
-        <tr>
-          <td>${book.name}</td>
-          <td>${book.author}</td>
-          <td>${book.nr_pages}</td>
-          <td><button class="btn btn-success">${book.status}</button></td>
-          <td><button class="btn btn-danger">delete</button></td>
-        </tr>
-        `;
-      tbody.insertAdjacentHTML("afterbegin", htmlBook);
-    });
+function deleteBook(currentBook) {
+    my_library.splice(currentBook, currentBook + 1);
 }
+
+function findBook(libraryArray, name) {
+    if (libraryArray.length === 0 || libraryArray === null) {
+      return;
+    }
+    for (book of libraryArray)
+      if (book.name === name) {
+        return libraryArray.indexOf(book);
+      }
+}
+
+function updateLocalStorage() {
+    localStorage.setItem("my_library", JSON.stringify(my_library));
+  }
+
+function checkLocalStorage() {
+    if (localStorage.getItem("library")) {
+      my_library = JSON.parse(localStorage.getItem("my_library"));
+    } else {
+      my_library = DEFAULT_DATA;
+    }
+  }
 
 render();
+
 
 
 
